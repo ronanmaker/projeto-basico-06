708488402e15b770db604f66d4a989b59032741b
Matuk <mateusmatuk@fai-mg.br>
2020-03-09 20:05:18 -0300

    segundo commit

--- src/br/fai/projeto06/Main.java
+++ src/br/fai/projeto06/Main.java
@@ -2,10 +2,33 @@ package br.fai.projeto06;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Main.app;
-		
-
-	}
-
+    public static void main(String[] args) {
+        
+        String[] carros = new String[4];
+        
+        carros[0] = "civic";
+        carros[1] = "gol";
+        carros[2] = "palio";
+        carros[3] = "uno";
+        
+        for(String carro : carros){
+            System.out.println(carro);
+        }
+        
+        carros[1] = "L200";
+        
+        System.out.println("\r\n");
+        
+        for(String carro : carros){
+            System.out.println(carro);
+        }
+        
+        carros[3] = "gol";
+        
+        System.out.println("\r\n");
+        
+        for(String carro : carros){
+            System.out.println(carro);
+        }
+    }
 }
